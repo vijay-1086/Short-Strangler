a7f7e5296e551b7cca427b5a2b30c020fbccec6f
vijay.ganesh <vijay_1086@hotmail.com>
2020-06-14 18:44:09 +0530

    Corrected fetching logic for bhavcopy and fovolt

--- src/services/bhavCopy.js
+++ src/services/bhavCopy.js
@@ -5,45 +5,92 @@ const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
 const unzipper = require("unzipper");
+const csv = require("csv-parser");
+const fs = require("fs");
+const _ = require("lodash");
 
-const csvPath = path.join(appRoot.path, "downloads");
+const CSV_PATH = path.join(appRoot.path, "downloads/files");
+let fileName;
+let results = [];
 
 function getUri() {
-  let uri = "https://www1.nseindia.com/content/historical/DERIVATIVES";
+    let uri = "https://www1.nseindia.com/content/historical/DERIVATIVES";
 
-  const date = getPreviousTradingDate(dayjs.utc());
-  const year = date.format("YYYY");
-  const month = date.format("MMM").toUpperCase();
-  const day = date.format("DD");
+    const date = getPreviousTradingDate(dayjs.utc());
+    const year = date.format("YYYY");
+    const month = date.format("MMM").toUpperCase();
+    const day = date.format("DD");
+    zipName = path.join(CSV_PATH, "/fo" + day + month + year + "bhav.csv.zip");
+    fileName = path.join(CSV_PATH, "/fo" + day + month + year + "bhav.csv");
+    uri += "/" + year + "/" + month + "/fo" + day + month + year + "bhav.csv.zip";
 
-  uri += "/" + year + "/" + month + "/fo" + day + month + year + "bhav.csv.zip";
-  return uri;
+    return uri;
 }
 
 function getPreviousTradingDate(date) {
-  const thisDate = dayjs(date).add(-1, "d");
-  const day = thisDate.format("d");
-  if (day == 6 || day == 0) return getPreviousTradingDate(thisDate);
+    const thisDate = dayjs(date).add(-1, "d");
+    const day = thisDate.format("d");
+    if (day == 6 || day == 0) return getPreviousTradingDate(thisDate);
 
-  return thisDate;
+    return thisDate;
 }
 
 // Todo: Load data into database
+function deleteFiles() {
+    fs.readdir(CSV_PATH, (error, files) => {
+        if (error)
+            console.log(error);
 
-function fetchBhavCopy() {
-  return axios({
-    url: getUri(),
-    method: "GET",
-    headers: { "user-agent": "Mozilla/5.0" },
-    responseType: "stream",
-  })
-    .then(function (response) {
-      response.data.pipe(unzipper.Extract({ path: csvPath }));
-    })
-    .catch((error) => {
-      console.log(error);
+        for (const file of files) {
+            fs.unlink(path.join(CSV_PATH, file), err => {
+                if (err)
+                    console.log(err);
+            })
+        }
     });
+    console.log("files deleted");
 }
 
-fetchBhavCopy();
+async function fetchBhavcopy() {
+    await axios({
+        url: getUri(),
+        method: 'GET',
+        headers: { "user-agent": "Mozilla/5.0" },
+        responseType: "stream"
+    })
+        .then((response) => {
+            console.log("response received");
+            response.data.pipe(unzipper.Extract({ path: CSV_PATH }));
+            console.log("file extracted");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function readBhavcopy() {
+    console.log("Read start");
+    const reader = fs.createReadStream(fileName);
+    reader.pipe(csv())
+        .on("data", (data) => {
+            results.push(data);
+        }).on("end", () => {
+            console.log(results.length);
+            console.log(_.last(results));
+        }).on("error", (error) => {
+            console.log(error);
+        })
+}
+
+
+async function loadBhavcopy() {
+    deleteFiles();
+    await fetchBhavcopy();
+
+    setTimeout(readBhavcopy, 1000);
+    console.log("method end");
+}
+
+loadBhavcopy();
+
 //module.exports.loadBhavCopy = loadBhavCopy;
--- src/services/fovolt.js
+++ src/services/fovolt.js
@@ -5,42 +5,66 @@ const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
 const fs = require("fs");
+const csv = require("csv-parser");
+const _ = require("lodash");
 
 const csvPath = path.join(appRoot.path, "downloads", "fovolt.csv");
+const results = [];
 
 function getUri() {
-  let uri = "https://www1.nseindia.com/archives/nsccl/volt/FOVOLT_";
+    let uri = "https://www1.nseindia.com/archives/nsccl/volt/FOVOLT_";
 
-  const date = getPreviousTradingDate(dayjs.utc());
-  const year = date.format("YYYY");
-  const month = date.format("MM").toUpperCase();
-  const day = date.format("DD");
+    const date = getPreviousTradingDate(dayjs.utc());
+    const year = date.format("YYYY");
+    const month = date.format("MM").toUpperCase();
+    const day = date.format("DD");
 
-  uri += day + month + year + ".csv";
-  return uri;
+    uri += day + month + year + ".csv";
+    return uri;
 }
 
 function getPreviousTradingDate(date) {
-  const thisDate = dayjs(date).add(-1, "d");
-  const day = thisDate.format("d");
-  if (day == 6 || day == 0) return getPreviousTradingDate(thisDate);
+    const thisDate = dayjs(date).add(-1, "d");
+    const day = thisDate.format("d");
+    if (day == 6 || day == 0) return getPreviousTradingDate(thisDate);
 
-  return thisDate;
+    return thisDate;
 }
 
-function fetchFovolt() {
-  return axios({
-    url: getUri(),
-    method: "GET",
-    headers: { "user-agent": "Mozilla/5.0" },
-    responseType: "stream",
-  })
-    .then((response) => {
-      response.data.pipe(fs.createWriteStream(csvPath));
+async function fetchFovolt() {
+    await axios({
+        url: getUri(),
+        method: "GET",
+        headers: { "user-agent": "Mozilla/5.0" },
+        responseType: "stream",
     })
-    .catch((error) => {
-      console.log(error);
-    });
+        .then((response) => {
+            //console.log(`Status: ${response.status}, Status Text: ${response.statusText}, data: ${response.data}`)
+            response.data.pipe(fs.createWriteStream(csvPath));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
-fetchFovolt();
+function readFovolt() {
+    console.log("Read start");
+    const reader = fs.createReadStream(csvPath);
+    reader.pipe(csv())
+        .on("data", (data) => {
+            results.push(data);
+        }).on("end", () => {
+            console.log(results.length);
+            console.log(_.last(results));
+        }).on("error", (error) => {
+            console.log(error);
+        })
+}
+
+async function loadFovolt() {
+    await fetchFovolt();
+    setTimeout(readFovolt, 1000);
+}
+
+loadFovolt();
+
